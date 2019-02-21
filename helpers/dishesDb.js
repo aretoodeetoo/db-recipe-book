296a87c0cadcffdeae6fe827626e93a9d6fb3ece
croy <roy.christie95@gmail.com>
2019-02-21 17:45:31 -0500

    Created most dishesDb functions for get, getById, etc

---
+++ helpers/dishesDb.js
@@ -0,0 +1,38 @@
+const db = require('../dbConfig');
+
+module.exports = {
+    get,
+    getById,
+    getDishes,
+    insert,
+    update,
+    remove,
+};
+
+function get(){
+    return db('dishes');
+}
+
+function getById(id){
+    return db('dishes')
+        .where({ id })
+        .first();
+}
+
+function insert (dish){
+    return db('dishes')
+        .insert(dish)
+        .then(ids => 
+            { return getById(ids[0])}
+        );
+}
+
+function update(id, changes){
+    return db('dishes')
+        .where({ id })
+        .update(changes);
+}
+
+function remove(id){
+    return db('dishes')
+}
