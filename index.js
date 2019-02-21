55ac0e74ae9f711c72d0dd7c06bb23460394a69f
croy <roy.christie95@gmail.com>
2019-02-21 18:19:26 -0500

    Added functioning getRecipe endpoint

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ server.get('/api/dishes', async (req, res) => {
     }
 });
 
+// Get all recipes
+server.get('/api/recipes', async (req, res) => {
+    try {
+        const recipes = await db('recipes');
+        res.status(200).json(recipes);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+})
+
 const port = 3300;
 server.listen(port, function() {
     console.log(`Server listening on port 3300`);
